083aa2fbfb8834a0572f59264f9a7bb103e031e9
Rasmus Ros <rasmus.s.ros@gmail.com>
2018-12-15 15:53:40 +0100

    refactored localsearchsolver

--- common/src/main/kotlin/combo/sat/solvers/LocalSearch.kt
+++ common/src/main/kotlin/combo/sat/solvers/LocalSearch.kt
@@ -6,14 +6,12 @@ import combo.util.IntSet
 import combo.util.millis
 import kotlin.math.max
 import kotlin.math.min
-import kotlin.random.Random
 
 sealed class LocalSearch(val problem: Problem,
                          val propTable: UnitPropagationTable?,
                          val timeout: Long,
                          val maxRestarts: Int,
                          val maxSteps: Int,
-                         val maxSidewaySteps: Int,
                          val pRandomWalk: Double,
                          val maxConsideration: Int) {
     abstract val config: SolverConfig
@@ -59,37 +57,11 @@ class LocalSearchOptimizer<O : ObjectiveFunction>(problem: Problem,
                                                   timeout: Long = -1L,
                                                   maxRestarts: Int = 10,
                                                   maxSteps: Int = problem.nbrVariables,
-                                                  maxSidewaySteps: Int = max(10, maxSteps / 4),
                                                   pRandomWalk: Double = 0.05,
                                                   maxConsideration: Int = 32) :
-        LocalSearch(problem, propTable, timeout, maxRestarts, maxSteps, maxSidewaySteps, pRandomWalk, maxConsideration), Optimizer<O> {
+        LocalSearch(problem, propTable, timeout, maxRestarts, maxSteps, pRandomWalk, maxConsideration), Optimizer<O> {
 
     override fun optimizeOrThrow(function: O, contextLiterals: Literals): Labeling {
-        val end = if (timeout > 0L) millis() + timeout else Long.MAX_VALUE
-        var bestLabeling: Labeling? = null
-        var bestScore = Double.NEGATIVE_INFINITY
-        for (i in 1..maxRestarts) {
-            recordIteration()
-            val labeling = optIteration(contextLiterals, end)
-            if (labeling != null) {
-                val score = let {
-                    val d = function.value(labeling)
-                    if (config.maximize) d else -d
-                }
-                if (score > bestScore) {
-                    bestLabeling = labeling
-                    bestScore = score
-                }
-            }
-            if (millis() > end) break
-        }
-        recordCompleted(bestLabeling != null)
-        return bestLabeling ?: if (millis() > end) throw TimeoutException(timeout)
-        else throw IterationsReachedException(maxRestarts)
-    }
-
-    private fun optIteration(contextLiterals: Literals, end: Long): Labeling? {
-        // recordFlip
         TODO()
     }
 }
@@ -100,40 +72,13 @@ class LocalSearchSolver(problem: Problem,
                         timeout: Long = -1L,
                         maxRestarts: Int = 10,
                         maxSteps: Int = problem.nbrVariables,
-                        maxSidewaySteps: Int = max(10, maxSteps / 4),
                         pRandomWalk: Double = 0.05,
                         maxConsideration: Int = 32) :
-        LocalSearch(problem, propTable, timeout, maxRestarts, maxSteps, maxSidewaySteps, pRandomWalk, maxConsideration), Solver {
+        LocalSearch(problem, propTable, timeout, maxRestarts, maxSteps, pRandomWalk, maxConsideration), Solver {
 
 
     override fun witnessOrThrow(contextLiterals: Literals): Labeling {
         val end = if (timeout > 0L) millis() + timeout else Long.MAX_VALUE
-        for (i in 1..maxRestarts) {
-            recordIteration()
-            val labeling = satIteration(contextLiterals, end)
-            if (labeling != null) {
-                recordCompleted(true)
-                return labeling
-            }
-            if (millis() > end) throw TimeoutException(timeout)
-        }
-        recordCompleted(false)
-        throw IterationsReachedException(maxRestarts)
-    }
-
-
-    private fun satIteration(contextLiterals: Literals, end: Long): MutableLabeling? {
-        val rng = config.nextRandom()
-
-        val tracker = if (propTable != null)
-            PropLabelingTracker(config.labelingBuilder.build(problem.nbrVariables), problem, propTable, contextLiterals, rng)
-        else
-            FlipLabelingTracker(config.labelingBuilder.generate(problem.nbrVariables, rng), problem, contextLiterals)
-
-        val improvement = IntArray(
-                if (maxConsideration <= 0) problem.sentences.maxBy { it.literals.size }?.size ?: 0
-                else maxConsideration)
-
         val contextIxs = IntSet().apply {
             contextLiterals.forEach { i ->
                 this.add(i.asIx())
@@ -142,67 +87,67 @@ class LocalSearchSolver(problem: Problem,
                 sentences.forEach { j -> this.add(j.asIx()) }
             }
         }
+        val _maxCon = max(2, min(maxConsideration, problem.nbrVariables))
 
-        for (flips in 1..maxSteps) {
-            if (tracker.unsatisfied.isEmpty())
-                return tracker.labeling
+        for (restart in 1..maxRestarts) {
+            recordIteration()
+            val rng = config.nextRandom()
 
-            // Pick random unsatisfied clause
-            val pickedSentenceIx = tracker.unsatisfied.random(rng)
-            val pickedSentence = problem.sentences[pickedSentenceIx]
-            val literals = pickedSentence.literals
+            val tracker = if (propTable != null)
+                PropLabelingTracker(config.labelingBuilder.build(problem.nbrVariables), problem, propTable, contextLiterals, rng)
+            else
+                FlipLabelingTracker(config.labelingBuilder.generate(problem.nbrVariables, rng), problem, contextLiterals)
 
-            val ix = if (pRandomWalk > rng.nextDouble()) {
-                 IntPermutation(literals.size).firstOrNull {
-                     literals[it].asIx() !in contextIxs
-                 } ?: -1
-            } else chooseBest(tracker, contextIxs, literals, improvement, rng)
+            for (flips in 1..maxSteps) {
+                if (tracker.unsatisfied.isEmpty()) {
+                    recordCompleted(true)
+                    return tracker.labeling
+                }
 
-            if (ix < 0) continue
-            tracker.set(!tracker.labeling.asLiteral(ix))
-            tracker.updateUnsatisfied(tracker.labeling.asLiteral(ix))
+                // Pick random unsatisfied clause
+                val pickedSentenceIx = tracker.unsatisfied.random(rng)
+                val pickedSentence = problem.sentences[pickedSentenceIx]
+                val literals = pickedSentence.literals
 
-            recordFlip(pickedSentence, tracker.labeling.asLiteral(ix))
-            if (millis() > end) return null
+                val litIx = if (pRandomWalk > rng.nextDouble()) {
+                    IntPermutation(literals.size).firstOrNull {
+                        literals[it].asIx() !in contextIxs
+                    } ?: -1
+                } else {
+                    val litIxs = if (literals.size >= _maxCon) IntPermutation(literals.size, rng) else (0 until literals.size)
+                    litIxs.maxBy { i ->
+                        val ix = literals[i].asIx()
+                        if (ix in contextIxs)
+                            -1
+                        else {
+                            val sents = if (propTable != null) propTable.variableSentences[ix]
+                            else problem.sentencesWith(ix)
+                            val pre = sents.sumBy { if (it in tracker.unsatisfied) problem.sentences[it].flipsToSatisfy(tracker.labeling) else 0 }
+                            val lit = !tracker.labeling.asLiteral(ix)
+                            tracker.set(lit)
+                            val post = sents.sumBy { problem.sentences[it].flipsToSatisfy(tracker.labeling) }
+                            tracker.undo(lit)
+                            pre - post
+                        }
+                    } ?: -1
+                }
+                if (litIx < 0) continue
+                val ix = literals[litIx].asIx()
+                if (ix in contextIxs) continue
+                tracker.set(!tracker.labeling.asLiteral(ix))
+                tracker.updateUnsatisfied(tracker.labeling.asLiteral(ix))
+
+                recordFlip(pickedSentence, tracker.labeling.asLiteral(ix))
+                if (millis() > end) {
+                    recordCompleted(false)
+                    throw TimeoutException(timeout)
+                }
+            }
         }
-        return null
+
+        recordCompleted(false)
+        throw IterationsReachedException(maxRestarts)
     }
 
-    private fun chooseBest(tracker: LabelingTracker,
-                           contextIds: IntSet,
-                           literals: Literals,
-                           improvement: IntArray,
-                           rng: Random): Int {
-        val perm = if (literals.size >= improvement.size) IntPermutation(literals.size, rng) else null
-        for (i in 0 until min(improvement.size, literals.size)) {
-            improvement[i] = 0
-            val ix = literals[perm?.encode(i) ?: i].asIx()
-            if (ix in contextIds) {
-                improvement[i] = Int.MIN_VALUE
-                continue
-            }
-            val sents = if (propTable != null) propTable.variableSentences[ix]
-            else problem.sentencesWith(ix)
-            for (sentenceIx in sents) {
-                val sent = problem.sentences[sentenceIx]
-                improvement[i] += if (sentenceIx in tracker.unsatisfied) sent.flipsToSatisfy(tracker.labeling) else 0
-            }
-            val lit = !tracker.labeling.asLiteral(ix)
-            tracker.set(lit)
-            for (sentenceIx in sents) {
-                val sent = problem.sentences[sentenceIx]
-                improvement[i] -= sent.flipsToSatisfy(tracker.labeling)
-            }
-            tracker.undo(lit)
-        }
-        val imprIx = IntPermutation(min(literals.size, improvement.size), rng).let {
-            it.maxBy { i ->
-                improvement[i]
-            }!!
-        }
-        val ix = literals[perm?.encode(imprIx) ?: imprIx].asIx()
-        return if (improvement[imprIx] < 0) -1 // TODO check for sideways steps
-        else ix
-    }
 }
 
