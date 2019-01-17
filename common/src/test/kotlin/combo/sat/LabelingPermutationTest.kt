43c14dded53e9ee78818ce9c8de8efee54110d94
Rasmus Ros <rasmus.s.ros@gmail.com>
2019-01-17 16:10:38 +0100

    changed to labelingfactory

--- common/src/test/kotlin/combo/sat/LabelingPermutationTest.kt
+++ common/src/test/kotlin/combo/sat/LabelingPermutationTest.kt
@@ -14,14 +14,14 @@ class LabelingPermutationTest {
 
     @Test
     fun sequenceSize() {
-        assertEquals(2.0.pow(3).toInt(), LabelingPermutation.sequence(3, BitFieldLabelingBuilder(), Random.Default).count())
-        assertEquals(2.0.pow(4).toInt(), LabelingPermutation.sequence(4, BitFieldLabelingBuilder(), Random.Default).count())
+        assertEquals(2.0.pow(3).toInt(), LabelingPermutation.sequence(3, BitFieldLabelingFactory, Random.Default).count())
+        assertEquals(2.0.pow(4).toInt(), LabelingPermutation.sequence(4, BitFieldLabelingFactory, Random.Default).count())
     }
 
     @Test
     fun noRepetition() {
-        val list = LabelingPermutation.sequence(4, BitFieldLabelingBuilder(), Random.Default).toList()
-        val set = LabelingPermutation.sequence(4, BitFieldLabelingBuilder(), Random.Default).toSet()
+        val list = LabelingPermutation.sequence(4, BitFieldLabelingFactory, Random.Default).toList()
+        val set = LabelingPermutation.sequence(4, BitFieldLabelingFactory, Random.Default).toSet()
         assertEquals(set.size, list.size)
     }
 
