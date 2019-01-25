304c159e114d41eca6f436d6bffce913455153c0
Rasmus Ros <rasmus.s.ros@gmail.com>
2019-01-25 15:46:01 +0100

    Added documentation to bandit declarations.

--- common/src/main/kotlin/combo/bandit/Bandits.kt
+++ common/src/main/kotlin/combo/bandit/Bandits.kt
@@ -7,6 +7,10 @@ import combo.sat.ValidationException
 import combo.util.EMPTY_INT_ARRAY
 import kotlin.math.abs
 
+/**
+ * A bandit optimizes an online binary decision problem. All bandits in combo are multi-variate, ie. there are multiple
+ * binary decision variables.
+ */
 interface Bandit {
 
     fun chooseOrThrow(assumptions: Literals = EMPTY_INT_ARRAY): Labeling
@@ -18,15 +22,36 @@ interface Bandit {
                 null
             }
 
+    /**
+     * Add the results of a bandit evaluation. The [weight] can be used to increase the importance of the update, for
+     * binomial this is interpreted as the n-parameter.
+     */
     fun update(labeling: Labeling, result: Double, weight: Double = 1.0)
+
+    /**
+     * A sample of the total rewards obtained, for use in analysis and debugging.
+     */
     val rewards: DataSample
 }
 
+/**
+ * A [PredictionBandit] uses a machine learning model as part of the algorithm.
+ */
 interface PredictionBandit : Bandit {
+    /**
+     * The total absolute error obtained on a prediction before update.
+     */
     val trainAbsError: DataSample
+    /**
+     * The total absolute error obtained on a prediction after update.
+     */
     val testAbsError: DataSample
 
+    /**
+     * Evaluate the machine learning model on a [labeling].
+     */
     fun predict(labeling: Labeling): Double
+
     fun train(labeling: Labeling, result: Double, weight: Double)
 
     override fun update(labeling: Labeling, result: Double, weight: Double) {
