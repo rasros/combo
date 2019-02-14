d1a649b2cbde4a2b427a0106e428556257ae6b1e
Rasmus Ros <rasmus.s.ros@gmail.com>
2019-02-14 12:36:49 +0100

    Added test for hierarchical normal

--- common/src/test/kotlin/combo/bandit/univariate/UnivariatePosteriorsTest.kt
+++ common/src/test/kotlin/combo/bandit/univariate/UnivariatePosteriorsTest.kt
@@ -3,23 +3,20 @@ package combo.bandit.univariate
 import combo.math.*
 import combo.test.assertEquals
 import kotlin.random.Random
-import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertTrue
 
 class UnivariatePosteriorsTest {
 
-    private val pool = PooledVarianceEstimator(RunningVariance())
-
     @Test
     fun binomialUpdate() {
         val stat = BinomialPosterior.defaultPrior()
         val r = Random(1024)
-        val s1 = generateSequence { BinomialPosterior.sample(stat, pool, r) }.take(100).sample(RunningVariance())
+        val s1 = generateSequence { BinomialPosterior.sample(stat, null, r) }.take(100).sample(RunningVariance())
         assertEquals(.5, s1.mean, 0.1)
         assertEquals(50.0, s1.sum, 5.0)
         doubleArrayOf(1.0, 1.0, 0.0).forEach { BinomialPosterior.update(stat, it) }
-        val s2 = generateSequence { BinomialPosterior.sample(stat, pool, r) }.take(100).sample(RunningVariance())
+        val s2 = generateSequence { BinomialPosterior.sample(stat, null, r) }.take(100).sample(RunningVariance())
         assertEquals(3 / 5.0, s2.mean, 0.1)
         assertTrue(s2.variance < s1.variance)
     }
@@ -29,10 +26,10 @@ class UnivariatePosteriorsTest {
         val p = PoissonPosterior
         val stat = p.defaultPrior()
         val r = Random(19)
-        val s1 = generateSequence { p.sample(stat, pool, r) }.take(100).sample(RunningVariance())
+        val s1 = generateSequence { p.sample(stat, null, r) }.take(100).sample(RunningVariance())
         assertEquals(1.0, s1.mean, 5.0) // posterior has variance=100, so prior estimate is bad
         doubleArrayOf(3.0, 4.0, 0.0, 5.0, 0.0, 1.0, 0.0).forEach { p.update(stat, it) }
-        val s2 = generateSequence { p.sample(stat, pool, r) }.take(100).sample(RunningVariance())
+        val s2 = generateSequence { p.sample(stat, null, r) }.take(100).sample(RunningVariance())
         assertEquals(stat.mean, s2.mean, 0.5)
         assertTrue(s2.variance < s1.variance)
     }
@@ -42,10 +39,10 @@ class UnivariatePosteriorsTest {
         val p = GeometricPosterior
         val stat = p.defaultPrior()
         val r = Random(46978)
-        val s1 = generateSequence { p.sample(stat, pool, r) }.take(100).sample(RunningVariance())
+        val s1 = generateSequence { p.sample(stat, null, r) }.take(100).sample(RunningVariance())
         assertEquals(0.5, s1.mean, 0.1)
         doubleArrayOf(3.0, 4.0, 1.0, 3.0, 6.0, 2.0, 3.0).forEach { p.update(stat, it) }
-        val s2 = generateSequence { p.sample(stat, pool, r) }.take(100).sample(RunningVariance())
+        val s2 = generateSequence { p.sample(stat, null, r) }.take(100).sample(RunningVariance())
         assertEquals(1 / stat.mean, s2.mean, 0.2)
         assertTrue(s2.variance < s1.variance)
     }
@@ -55,7 +52,7 @@ class UnivariatePosteriorsTest {
         val p = NormalPosterior
         val stat = p.defaultPrior()
         val r = Random(893)
-        val dataSample = generateSequence { p.sample(stat, pool, r) }.take(100).sample(FullSample())
+        val dataSample = generateSequence { p.sample(stat, null, r) }.take(100).sample(FullSample())
         val neg = dataSample.toArray().count { it < 0 }
         val pos = dataSample.toArray().count { it > 0 }
         assertEquals(neg.toDouble(), pos.toDouble(), 30.0)
@@ -68,7 +65,7 @@ class UnivariatePosteriorsTest {
             // Ignoring the priors here
             val v = RunningVariance()
             data.forEach { v.accept(it) }
-            val s = generateSequence { NormalPosterior.sample(v, pool, rng) }.take(100).sample(RunningVariance())
+            val s = generateSequence { NormalPosterior.sample(v, null, rng) }.take(100).sample(RunningVariance())
             assertEquals(v.mean, s.mean, 0.1)
 
             val alpha = v.nbrWeightedSamples / 2.0
@@ -91,7 +88,7 @@ class UnivariatePosteriorsTest {
         val variance = 10.0
         for (i in 1..1) p.update(stat, r.nextLogNormal(mean, variance))
         val v = RunningVariance()
-        for (i in 1..1000000) v.accept(p.sample(stat, pool, r))
+        for (i in 1..1000000) v.accept(p.sample(stat, null, r))
     }
 
     @Test
@@ -99,9 +96,9 @@ class UnivariatePosteriorsTest {
         val p = ExponentialPosterior
         val stat = p.defaultPrior()
         val r = Random(75)
-        val s1 = generateSequence { p.sample(stat, pool, r) }.take(50).sample(RunningVariance())
+        val s1 = generateSequence { p.sample(stat, null, r) }.take(50).sample(RunningVariance())
         doubleArrayOf(3.0, 4.0, 1.0, 3.0, 6.0, 2.0, 2.0).forEach { p.update(stat, it) }
-        val s2 = generateSequence { p.sample(stat, pool, r) }.take(50).sample(RunningVariance())
+        val s2 = generateSequence { p.sample(stat, null, r) }.take(50).sample(RunningVariance())
         assertEquals(1 / 3.0, s2.mean, 0.1)
         assertTrue(s2.variance < s1.variance)
     }
@@ -111,24 +108,24 @@ class UnivariatePosteriorsTest {
         val p = GammaScalePosterior(fixedShape = 2.0)
         val stat = p.defaultPrior()
         val r = Random(1023)
-        val s1 = generateSequence { p.sample(stat, pool, r) }.take(50).sample(RunningVariance())
+        val s1 = generateSequence { p.sample(stat, null, r) }.take(50).sample(RunningVariance())
         doubleArrayOf(3.0, 4.0, 1.0, 3.0, 6.0, 2.0, 2.0).forEach { p.update(stat, it) }
-        val s2 = generateSequence { p.sample(stat, pool, r) }.take(50).sample(RunningVariance())
+        val s2 = generateSequence { p.sample(stat, null, r) }.take(50).sample(RunningVariance())
         assertEquals(2.0 / 3.0, s2.mean, 0.1)
         assertTrue(s2.variance < s1.variance)
     }
 
-
     @Test
-    @Ignore
     fun properPriors() {
         for ((i, posterior) in arrayOf(PoissonPosterior, ExponentialPosterior, NormalPosterior, GammaScalePosterior(0.1),
                 BinomialPosterior, GeometricPosterior, LogNormalPosterior).withIndex()) {
-            val prior = posterior.defaultPrior()
+            @Suppress("UNCHECKED_CAST")
+            val post = posterior as UnivariatePosterior<VarianceEstimator>
+            val prior = post.defaultPrior()
             assertTrue(prior.variance.isFinite(), i.toString())
             assertTrue(prior.mean.isFinite(), i.toString())
             val s = RunningVariance()
-            //TODO generateSequence { posterior.sample(prior, pool, Random) }.take(50).forEach { s.accept(it) }
+            generateSequence { post.sample(prior, null, Random) }.take(50).forEach { s.accept(it) }
             assertTrue(s.variance.isFinite(), i.toString())
             assertTrue(s.variance > 0, i.toString())
             assertTrue(s.mean.isFinite(), i.toString())
@@ -139,5 +136,37 @@ class UnivariatePosteriorsTest {
         }
     }
 
-    // TODO tests for hierarchical
+    @Test
+    fun hierarchicalNormalShrinkage() {
+        val groups = 8
+        val dataPoints = 5
+        val experiments = 2
+
+        // This test varies the error of two sets of groups.
+        // The estimated mean of the second group should be shrunk towards the pool mean because the error is larger
+        // there.
+        val weights = DoubleArray(groups) { 1 + it.toDouble() }
+        val posterior = HierarchicalNormalPosterior
+
+        val rng = Random(0)
+        val pools = Array(experiments) { posterior.poolEstimator(posterior.defaultPrior()) }
+        val data = Array(experiments) {
+            val err = 10 * it.toDouble() + 0.5
+            val pool = pools[it]
+            Array(weights.size) { i ->
+                RunningVariance(1 + i.toDouble(), err * dataPoints, dataPoints.toDouble()).also { pool.addArm(it) }
+            }.also { pool.recalculate() }
+        }
+
+        val samples = Array(experiments) { i ->
+            Array(weights.size) { j ->
+                generateSequence { posterior.sample(data[i][j], pools[i], rng) }.take(100).sample(RunningVariance())
+            }
+        }
+
+        val means = samples.map { it.map { it.mean }.toTypedArray().asSequence().sample(RunningVariance()) }
+        assertEquals((groups + 1) / 2.0, means[0].mean, 0.1)
+        assertEquals((groups + 1) / 2.0, means[1].mean, 0.1)
+        assertTrue(means[0].variance > means[1].variance)
+    }
 }
