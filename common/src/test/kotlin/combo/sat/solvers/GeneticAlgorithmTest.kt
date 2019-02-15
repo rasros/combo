852d74b7c01cdff09f2ba792b2f93490ad0e1943
Rasmus Ros <rasmus.s.ros@gmail.com>
2019-02-15 18:22:34 +0100

    tweaked parameters for GASolverTest

--- common/src/test/kotlin/combo/sat/solvers/GeneticAlgorithmTest.kt
+++ common/src/test/kotlin/combo/sat/solvers/GeneticAlgorithmTest.kt
@@ -1,7 +1,5 @@
 package combo.sat.solvers
 
-import combo.math.FastGAMutation
-import combo.math.TournamentElimination
 import combo.sat.Problem
 import combo.sat.RandomInitializer
 import combo.sat.WeightInitializer
@@ -16,7 +14,7 @@ class GALinearOptimizerTest : LinearOptimizerTest() {
     }
 
     override fun infeasibleOptimizer(problem: Problem) = GAOptimizer<LinearObjective>(problem).apply {
-        randomSeed = 0L; timeout = 5 * 1000L; maxSteps = 1; timeout = 1
+        randomSeed = 0L; timeout = 1; maxSteps = 1
     }
 }
 
@@ -32,13 +30,11 @@ class GASolverTest : SolverTest() {
     }
 
     override fun largeSolver(problem: Problem) = GASolver(problem).apply {
-        randomSeed = 0L; timeout = 5 * 1000L; candidateSize = 50; maxSteps = 500; restartKeeps = 0.8; stallSteps = 100
-        mutationProbability = 1.0; mutation = FastGAMutation(problem.nbrVariables, beta = 5.0); elimination = TournamentElimination(5)
+        randomSeed = 0L; timeout = 5 * 1000L; candidateSize = 100
     }
 
     override fun unsatSolver(problem: Problem) = GASolver(problem).apply {
-        randomSeed = 0L; maxSteps = 1; timeout = 1; restarts = 1
-
+        randomSeed = 0L; timeout = 1L; maxSteps = 1
     }
 }
 
