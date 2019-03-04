889b96afe8120b78e38356fb24658b02a151319f
Rasmus Ros <rasmus.s.ros@gmail.com>
2019-03-04 13:23:13 +0100

    Changed format of literals to Dimacs

--- common/src/main/kotlin/combo/sat/Literals.kt
+++ common/src/main/kotlin/combo/sat/Literals.kt
@@ -3,29 +3,20 @@
 package combo.sat
 
 import kotlin.jvm.JvmName
-
-import kotlin.math.abs
-
-/**
- * An [Ix] represents a variable index, starting from 0 to the number of variables.
- */
-typealias Ix = Int
+import kotlin.math.absoluteValue
 
 /**
- * A literal is a variable with truth and false values. The [Ix] is left bit shifted one step and the truth value
+ * A literal is a variable with truth and false values. The index is left bit shifted one step and the truth value
  * is stored in the last bit.
  */
 typealias Literal = Int
 
-fun Ix.toLiteral(truth: Boolean): Literal = this shl 1 xor if (truth) 0 else 1
-fun Literal.toBoolean() = this and 1 == 0
-fun Literal.toIx(): Ix = this shr 1
-operator fun Literal.not(): Literal = this xor 1
-
-fun Literal.toDimacs() = (toIx() + 1) * if (toBoolean()) 1 else -1
-fun Literal.fromDimacs() = (abs(this) - 1).toLiteral(this >= 0)
+fun Int.toLiteral(truth: Boolean): Literal = if (truth) this + 1 else -(this + 1)
+fun Literal.toBoolean() = this > 0
+fun Literal.toIx(): Int = this.absoluteValue - 1
+fun Literal.offset(offset: Int) = (toIx() + offset).toLiteral(toBoolean())
+operator fun Literal.not(): Literal = -this
 
 typealias Literals = IntArray
 
-fun Boolean.toInt() = if (this) 1 else 0
 
