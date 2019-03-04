41f6c4f5a0f1c13385ba00e4fe94477e61912fd1
Rasmus Ros <rasmus.s.ros@gmail.com>
2019-03-04 13:23:50 +0100

    added parameter to unitPropagation to return adjusted constraints or not

--- common/src/main/kotlin/combo/sat/Problem.kt
+++ common/src/main/kotlin/combo/sat/Problem.kt
@@ -1,7 +1,6 @@
 package combo.sat
 
-import combo.util.IntList
-import combo.util.IntSet
+import combo.util.*
 
 /**
  * This class sufficiently describes a SAT problem, with the constraints and a count of the number of variables. It also
@@ -11,20 +10,23 @@ class Problem(val constraints: Array<out Constraint>, val nbrVariables: Int) {
 
     val nbrConstraints get() = constraints.size
 
-    private val variableConstraints: Array<IntArray> = Array(nbrVariables) { IntList() }.let { lists ->
+    private val variableConstraints: IntHashMap<IntArray> = let {
+        val map = IntHashMap<IntList>(nullKey = -1)
         for ((i, cons) in constraints.withIndex()) {
             for (lit in cons) {
                 val ix = lit.toIx()
-                lists[ix].add(i)
+                assert(ix < nbrVariables)
+                if (!map.containsKey(ix)) map[ix] = IntList()
+                map[ix]!!.add(i)
             }
         }
-        Array(nbrVariables) { lists[it].toArray() }
+        map.map { it.toArray() }
     }
 
     /**
      * Returns the index into the [constraints] array of all constraints with the given variable.
      */
-    fun constraintsWith(varIx: Ix) = variableConstraints[varIx]
+    fun constraintsWith(varIx: Int) = variableConstraints[varIx] ?: EMPTY_INT_ARRAY
 
     fun satisfies(l: Instance) = constraints.all { it.satisfies(l) }
 
@@ -35,9 +37,9 @@ class Problem(val constraints: Array<out Constraint>, val nbrVariables: Int) {
      * New units will be added to the set. This method does not change the original problem but returns a reduced
      * problem.
      */
-    fun unitPropagation(units: IntSet = IntSet()): Array<Constraint> {
+    fun unitPropagation(units: IntHashSet = IntHashSet(), returnConstraints: Boolean = false): Array<Constraint> {
 
-        fun addUnit(units: IntSet, unit: Literal): Boolean {
+        fun addUnit(units: IntHashSet, unit: Literal): Boolean {
             if (units.contains(!unit)) throw UnsatisfiableException("Unsatisfiable by unit propagation.", literal = unit)
             else return units.add(unit)
         }
@@ -62,18 +64,21 @@ class Problem(val constraints: Array<out Constraint>, val nbrVariables: Int) {
                 val unitId = unitLit.toIx()
                 val matching = constraintsWith(unitId)
                 for (i in matching.indices) {
-                    val reduced = (copy[matching[i]] ?: constraints[matching[i]]).propagateUnit(unitLit)
+                    val reduced = (copy[matching[i]] ?: constraints[matching[i]]).unitPropagation(unitLit)
                     copy[matching[i]] = reduced
                     if (reduced.isUnit())
                         if (reduced.literals.any { l -> addUnit(units, l) }) unitsIx.add(matching[i])
                 }
             }
         }
-        return copy.asSequence()
-                .mapIndexed { i, it -> it ?: constraints[i] }
-                .filter { !it.isUnit() && it != Tautology }
-                .toList()
-                .toTypedArray()
+        return if (returnConstraints) {
+            copy.asSequence()
+                    .mapIndexed { i, it -> it ?: constraints[i] }
+                    .filter { !it.isUnit() && it != Tautology }
+                    .toMutableList().apply { add(Conjunction(collectionOf(*units.toArray()))) }
+                    .toTypedArray()
+        } else emptyArray()
     }
 }
+
 
