2247fb870fcbb84350dd11cb3a9008c1f0cea86d
Rasmus Ros <rasmus.s.ros@gmail.com>
2019-05-16 11:22:55 +0200

    Added efficient implementation of transitive closure of graph calculation, based on tarjans algorithm

---
+++ common/src/main/kotlin/combo/math/ImplicationDigraph.kt
@@ -0,0 +1,151 @@
+package combo.math
+
+import combo.sat.*
+import combo.sat.constraints.*
+import combo.util.IntHashSet
+import combo.util.IntList
+import combo.util.isNotEmpty
+import kotlin.math.min
+
+/**
+ * This class calculates the transitive closure of a graph in adjacency list form.
+ * This is a recursive free implementation of the Simple_SC algorithm by Esko Nuutilas,
+ * see http://www.cs.hut.fi/~enu/thesis.html
+ */
+class ImplicationDigraph(val nbrVariables: Int, implications: Map<Int, IntArray>) {
+
+    /**
+     * This constructor calculates the initial edges in the graph.
+     */
+    constructor(problem: Problem) : this(problem.nbrVariables, HashMap<Int, IntHashSet>().let { map ->
+        for (constraint in problem.constraints) {
+            if (constraint is Disjunction && constraint.size == 2) {
+                val array = constraint.literals.toArray()
+                map.getOrPut(!array[0]) { IntHashSet() }.add(array[1])
+                map.getOrPut(!array[1]) { IntHashSet() }.add(array[0])
+            } else if ((constraint is Cardinality && constraint.degree == 1 && constraint.relation == Relation.LE) ||
+                    (constraint is Cardinality && constraint.degree == 2 && constraint.relation == Relation.LT)) {
+                val array = constraint.literals.toArray()
+                for (i in array.indices) {
+                    for (j in (i + 1) until array.size) {
+                        map.getOrPut(array[i]) { IntHashSet() }.add(!array[j])
+                        map.getOrPut(array[j]) { IntHashSet() }.add(!array[i])
+                    }
+                }
+            } else if (constraint is ReifiedEquivalent && constraint.constraint is Disjunction) {
+                for (literal in constraint.constraint.literals) {
+                    map.getOrPut(literal) { IntHashSet() }.add(constraint.literal)
+                    map.getOrPut(!constraint.literal) { IntHashSet() }.add(!literal)
+                }
+            } else if (constraint is ReifiedEquivalent && constraint.constraint is Conjunction) {
+                for (literal in constraint.constraint.literals) {
+                    map.getOrPut(!literal) { IntHashSet() }.add(!constraint.literal)
+                    map.getOrPut(constraint.literal) { IntHashSet() }.add(literal)
+                }
+            }
+        }
+        map.mapValues { it.value.toArray() }
+    })
+
+    private val trueImplications = arrayOfNulls<SparseBitArray?>(nbrVariables * 2)
+    private val falseImplications = arrayOfNulls<SparseBitArray?>(nbrVariables * 2)
+
+    fun trueImplications(literal: Int) = trueImplications[toArrayIndex(literal)]
+    fun falseImplications(literal: Int) = falseImplications[toArrayIndex(literal)]
+
+    fun toArray(literal: Int): IntArray {
+        val set = IntHashSet()
+        trueImplications[toArrayIndex(literal)]?.forEach { set.add(it.toLiteral(true)) }
+        falseImplications[toArrayIndex(literal)]?.forEach { set.add(it.toLiteral(false)) }
+        return set.toArray()
+    }
+
+    init {
+        for ((from, edges) in implications) {
+            val ix = toArrayIndex(from)
+            for (to in edges) {
+                if (to.toBoolean()) {
+                    if (trueImplications[ix] == null) trueImplications[ix] = SparseBitArray(nbrVariables)
+                    trueImplications[ix]!![to.toIx()] = true
+                } else {
+                    if (falseImplications[ix] == null) falseImplications[ix] = SparseBitArray(nbrVariables)
+                    falseImplications[ix]!![to.toIx()] = true
+                }
+            }
+        }
+
+        val visited = BooleanArray(nbrVariables * 2)
+        val component = IntArray(nbrVariables * 2) { -1 }
+        val root = IntArray(nbrVariables * 2)
+
+        var components = 0
+        val stack = IntList()
+
+        // This is a stack simulation to avoid recursion and stack overflow for large graphs (n>10000)
+        val callStack = IntList()
+
+        for (v in 0 until nbrVariables * 2) {
+            if (visited[v]) continue
+            callStack.add(v)
+            callStack.add(0)
+            while (callStack.isNotEmpty()) {
+                val i = callStack.removeAt(callStack.size - 1)
+                val v1 = callStack.removeAt(callStack.size - 1)
+                if (i == 0) {
+                    visited[v1] = true
+                    root[v1] = v1
+                    stack.add(v1)
+                }
+                var recurse = false
+                for (j in i until (implications[toLiteral(v1)]?.size ?: 0)) {
+                    val w = toArrayIndex(implications[toLiteral(v1)]!![j])
+                    if (!visited[w]) {
+                        callStack.add(v1)
+                        callStack.add(j + 1)
+                        callStack.add(w)
+                        callStack.add(0)
+                        recurse = true
+                        break
+                    }
+                    if (component[w] < 0) root[v1] = min(root[v1], root[w])
+                    addImplications(v1, w)
+                }
+                if (recurse) continue
+                if (root[v1] == v1) {
+                    var w: Int
+                    do {
+                        w = stack.removeAt(stack.size - 1)
+                        component[w] = components
+
+                        val ti = trueImplications[v1]
+                        val fi = falseImplications[v1]
+                        trueImplications[w] = ti
+                        falseImplications[w] = fi
+
+                        // If a variable and its negation belong to the same strongly connected component, ie. x <=> !x
+                        if (component[w] == component[w xor 1])
+                            throw UnsatisfiableException("Unsatisfiable during initialization of implication graph (2-sat).")
+
+                    } while (w != v1)
+                    components++
+                }
+                if (callStack.isNotEmpty()) {
+                    val w = v1
+                    val v2 = callStack[callStack.size - 2]
+                    root[v2] = min(root[v2], root[w])
+                    addImplications(v2, w)
+                }
+            }
+        }
+    }
+
+    private fun addImplications(to: Int, from: Int) {
+        if (trueImplications[to] == null && trueImplications[from] != null) trueImplications[to] = SparseBitArray(nbrVariables)
+        if (trueImplications[from] != null) trueImplications[to]!!.or(trueImplications[from]!!)
+        if (falseImplications[to] == null && falseImplications[from] != null) falseImplications[to] = SparseBitArray(nbrVariables)
+        if (falseImplications[from] != null) falseImplications[to]!!.or(falseImplications[from]!!)
+    }
+
+    private fun toArrayIndex(literal: Int) = (literal.toIx() shl 1) + if (literal.toBoolean()) 0 else 1
+    private fun toLiteral(arrayIndex: Int) = (arrayIndex shr 1).toLiteral(arrayIndex and 1 == 0)
+}
---
+++ common/src/test/kotlin/combo/math/ImplicationDigraphTest.kt
@@ -0,0 +1,113 @@
+package combo.math
+
+import combo.model.Model
+import combo.sat.*
+import combo.sat.constraints.Cardinality
+import combo.test.assertContentEquals
+import combo.util.mapArray
+import kotlin.random.Random
+import kotlin.test.Test
+import kotlin.test.assertFailsWith
+import kotlin.test.assertTrue
+
+class ImplicationDigraphTest {
+
+    @Test
+    fun unsatisfiedByImplicationDigraph() {
+        val problem = Model.model("Simple 2-Unsat") {
+            val f1 = bool()
+            val f2 = bool()
+            constraint { f1 or f2 }
+            constraint { f1 or !f2 }
+            constraint { !f1 or f2 }
+            constraint { !f1 or !f2 }
+        }.problem
+
+        assertFailsWith(UnsatisfiableException::class) { ImplicationDigraph(problem) }
+    }
+
+    @Test
+    fun simpleGraph() {
+        val id = ImplicationDigraph(10, mapOf(
+                1 to intArrayOf(2, 6, 8),
+                2 to intArrayOf(3, 1),
+                3 to intArrayOf(4, 2),
+                4 to intArrayOf(5),
+                5 to intArrayOf(4),
+                6 to intArrayOf(7),
+                7 to intArrayOf(6, 4),
+                8 to intArrayOf(9),
+                9 to intArrayOf(8, 10, 5, 3)
+        ))
+
+        assertContentEquals(intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), id.toArray(1).apply { sort() })
+        assertContentEquals(intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), id.toArray(2).apply { sort() })
+        assertContentEquals(intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), id.toArray(3).apply { sort() })
+        assertContentEquals(intArrayOf(4, 5), id.toArray(4).apply { sort() })
+        assertContentEquals(intArrayOf(4, 5), id.toArray(5).apply { sort() })
+        assertContentEquals(intArrayOf(4, 5, 6, 7), id.toArray(6).apply { sort() })
+        assertContentEquals(intArrayOf(4, 5, 6, 7), id.toArray(7).apply { sort() })
+        assertContentEquals(intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), id.toArray(8).apply { sort() })
+        assertContentEquals(intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), id.toArray(9).apply { sort() })
+        assertContentEquals(intArrayOf(), id.toArray(10).apply { sort() })
+    }
+
+    @Test
+    fun doubleLinearChainGraph() {
+        val problem = Model.model("Flag chain") {
+            var next = this
+            for (k in 1..100)
+                next = next.model("$k") {}
+        }.problem
+
+        val id = ImplicationDigraph(problem)
+
+        for (i in 0 until problem.nbrVariables) {
+            assertContentEquals((1 until (i + 1)).toList().toIntArray(),
+                    id.toArray(i.toLiteral(true)).apply { sort() })
+            assertContentEquals(((i + 2)..problem.nbrVariables).toList().toIntArray().mapArray { -it }.apply { sort() },
+                    id.toArray(i.toLiteral(false)).apply { sort() })
+        }
+    }
+
+    @Test
+    fun sat2Digraph() {
+        val problem = Model.model {
+            bool("x1")
+            model("x2") {
+                bool("x3")
+            }
+            bool("x4")
+            constraint { "x2" or "x4" }
+        }.problem
+        val id = ImplicationDigraph(problem)
+        for (i in 0 until 10) {
+            val instance = BitArray(problem.nbrVariables).also { RandomSet().initialize(it, Tautology, Random, null) }
+            for (j in IntPermutation(problem.nbrVariables, Random)) {
+                val lit = instance.literal(j)
+                id.trueImplications(lit)?.run { instance.or(this) }
+                id.falseImplications(lit)?.run { instance.andNot(this) }
+            }
+            assertTrue(problem.satisfies(instance))
+        }
+    }
+
+    @Test
+    fun cardinalityExclusiveDigraph() {
+        val problem = Model.model {
+            alternative(values = *Array(100) { it })
+        }.problem
+        val card = problem.constraints.first { it is Cardinality }
+        val id = ImplicationDigraph(problem)
+
+        for (i in 0 until 10) {
+            val instance = BitArray(problem.nbrVariables).also { RandomSet().initialize(it, Tautology, Random, null) }
+            for (j in IntPermutation(problem.nbrVariables, Random)) {
+                val lit = instance.literal(j)
+                id.trueImplications(lit)?.run { instance.or(this) }
+                id.falseImplications(lit)?.run { instance.andNot(this) }
+            }
+            assertTrue(card.satisfies(instance))
+        }
+    }
+}
