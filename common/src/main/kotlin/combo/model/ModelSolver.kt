b44ce2320c12cde26d37f456f6ae0880d17cbb41
Rasmus Ros <rasmus.s.ros@gmail.com>
2019-05-22 14:34:34 +0200

    changed assumptions from intarray to intcollection

--- common/src/main/kotlin/combo/model/ModelSolver.kt
+++ common/src/main/kotlin/combo/model/ModelSolver.kt
@@ -3,6 +3,8 @@ package combo.model
 
 import combo.sat.solvers.Solver
 import combo.util.EMPTY_INT_ARRAY
+import combo.util.EmptyCollection
+import combo.util.IntCollection
 import combo.util.IntHashSet
 
 class ModelSolver(val model: Model, val solver: Solver) {
@@ -19,10 +21,10 @@ class ModelSolver(val model: Model, val solver: Solver) {
     fun asSequence(vararg assumptions: Literal) =
             model.toAssignments(solver.asSequence(assumptionsLiterals(assumptions)))
 
-    private fun assumptionsLiterals(assumptions: Array<out Literal>): IntArray {
-        if (assumptions.isEmpty()) return EMPTY_INT_ARRAY
+    private fun assumptionsLiterals(assumptions: Array<out Literal>): IntCollection {
+        if (assumptions.isEmpty()) return EmptyCollection
         val set = IntHashSet()
         assumptions.forEach { it.toAssumption(model.index, set) }
-        return set.toArray().apply { sort() }
+        return set
     }
 }
