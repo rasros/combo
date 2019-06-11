6403edb4e454d6db6e4805aac719e932e941a6e9
Rasmus Ros <rasmus.s.ros@gmail.com>
2019-06-11 10:23:38 +0200

    removed some unused tests for datasamples

--- common/src/test/kotlin/combo/math/DataSamplesTest.kt
+++ common/src/test/kotlin/combo/math/DataSamplesTest.kt
@@ -1,48 +1,29 @@
 package combo.math
 
-import kotlin.random.Random
+import combo.test.assertContentEquals
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class DataSamplesTest {
+
     @Test
-    fun emptyReservoirSample() {
-        assertEquals(0, ReservoirSample(10, Random).toArray().size)
+    fun emptyGrowingSample() {
+        assertEquals(0, BucketSample(10).values().size)
+        assertEquals(0, BucketSample(10).labels().size)
     }
 
     @Test
-    fun reservoirSampleFull() {
-        val s = ReservoirSample(10, Random)
-        for (i in 1..10)
-            s.accept(i.toFloat())
-        val collect = s.toArray()
-        for (i in 0 until 10)
-            assertEquals(i.toFloat() + 1.0f, collect[i])
-    }
-
-    @Test
-    fun reservoirSampleUnderSize() {
-        val s = ReservoirSample(100, Random)
-        for (i in 1..10)
-            s.accept(i.toFloat())
-        val collect = s.toArray()
-        for (i in 0 until 10)
-            assertEquals(i.toFloat() + 1.0f, collect[i])
-        assertEquals(10, collect.size)
-    }
-
-    @Test
-    fun reservoirSampleOverSize() {
-        val s = ReservoirSample(20, Random)
+    fun growingSample() {
+        val s = BucketSample(10)
         for (i in 1..100)
             s.accept(i.toFloat())
-        val collect = s.toArray()
-        assertEquals(20, collect.size)
+        assertContentEquals(floatArrayOf(8.5f, 24.5f, 40.5f, 56.5f, 72.5f, 88.5f), s.values())
+        assertContentEquals(longArrayOf(16, 32, 48, 64, 80, 96), s.labels())
     }
 
     @Test
     fun emptyFullSample() {
-        assertEquals(0, FullSample().toArray().size)
+        assertEquals(0, FullSample().values().size)
     }
 
     @Test
@@ -50,61 +31,6 @@ class DataSamplesTest {
         val s = FullSample()
         for (i in 1..100)
             s.accept(i.toFloat())
-        val collect = s.toArray()
-        assertEquals(100, collect.size)
-        for (i in 1..100)
-            s.accept(i.toFloat())
-    }
-
-    @Test
-    fun bucketsSampleSizeOne() {
-        val s = BucketsSample(1)
-        for (i in 1..100)
-            s.accept(i.toFloat())
-        val data = s.toArray()
-        for (i in 1..100)
-            assertEquals(i.toFloat(), data[i - 1])
-    }
-
-    @Test
-    fun bucketsBig() {
-        val s = BucketsSample(10)
-        for (i in 1..200)
-            s.accept(i.toFloat())
-        val data = s.toArray()
-        assertEquals(20, data.size)
-        for (i in 0 until 20)
-            assertEquals(5.5f + i * 10.0f, data[i])
-    }
-
-    @Test
-    fun emptyBucketsSample() {
-        assertEquals(0, BucketsSample(10).toArray().size)
-    }
-
-    @Test
-    fun windowSampleFullSize() {
-        val s = WindowSample(20)
-        for (i in 1..20)
-            s.accept(i.toFloat())
-        val data = s.toArray()
-        assertEquals(20, data.size)
-        for (i in 1..20)
-            assertEquals(i.toFloat(), data[i - 1])
-    }
-
-    @Test
-    fun windowSampleReUse() {
-        val s = WindowSample(20)
-        for (i in 1..94)
-            s.accept(i.toFloat())
-        s.accept(1.0f)
-        val data2 = s.toArray()
-        assertEquals(1.0f, data2[data2.size - 1])
-    }
-
-    @Test
-    fun emptyWindowSample() {
-        assertEquals(0, WindowSample(10).toArray().size)
+        assertContentEquals(FloatArray(100) { (1 + it).toFloat() }, s.values())
     }
 }
