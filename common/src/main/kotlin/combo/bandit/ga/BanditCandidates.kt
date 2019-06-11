d171ab99c361e300ce569f78432d89baa8a25831
Rasmus Ros <rasmus.s.ros@gmail.com>
2019-06-11 10:21:02 +0200

    refactored banditcandidates

--- common/src/main/kotlin/combo/bandit/ga/BanditCandidates.kt
+++ common/src/main/kotlin/combo/bandit/ga/BanditCandidates.kt
@@ -1,51 +1,128 @@
 package combo.bandit.ga
 
+import combo.bandit.univariate.BanditPolicy
 import combo.ga.Candidates
 import combo.math.VarianceEstimator
 import combo.sat.Instance
-import combo.sat.MutableInstance
 import combo.util.IntHashSet
 import combo.util.isEmpty
+import combo.util.removeAt
+import kotlin.math.max
+import kotlin.math.min
 
 /**
  * All state of the search is kept here. The score is defined as mean for genetic operators that use score directly.
  */
-class BanditCandidates<E : VarianceEstimator>(override val instances: Array<MutableInstance>,
-                                              val baseData: E) : Candidates {
+class BanditCandidates<E : VarianceEstimator>(instances: Array<Instance>,
+                                              var minSamples: Float,
+                                              var maximize: Boolean,
+                                              val banditPolicy: BanditPolicy<E>) : Candidates {
 
-    private val origins: LongArray = LongArray(instances.size)
+    override var instances = instances
+        private set
+    var origins: LongArray = LongArray(instances.size)
+        private set
     private var oldestOrigin: Long = 0L
     override var oldestCandidate: Int = 0
         private set
 
-    override var minScore: Float = baseData.mean
-        private set
-    override var maxScore: Float = baseData.mean
-        private set
+    private var minMean = Float.POSITIVE_INFINITY
+    private var maxMean = Float.NEGATIVE_INFINITY
 
-    override val nbrCandidates: Int
-        get() = instances.size
-    override val nbrVariables: Int
-        get() = instances[0].size
+    override val bestScore: Float get() = if (maximize) -maxMean else minMean
+    override val worstScore: Float get() = if (maximize) -minMean else maxMean
 
-    var nbrUpdates = 0L
+    override val nbrCandidates: Int get() = instances.size
+    override val nbrVariables: Int get() = instances[0].size
+
+    var step = 0L
         private set
 
     val estimators: MutableMap<Instance, E> = HashMap()
-    val duplications: MutableMap<Instance, IntHashSet> = HashMap()
+    private val duplications: MutableMap<Instance, IntHashSet> = HashMap()
 
     init {
-        @Suppress("UNCHECKED_CAST")
         for (i in instances.indices)
-            addCandidate(i, baseData.copy() as E)
+            indexCandidate(i) { banditPolicy.baseData() }
+    }
+
+    override fun score(ix: Int, elimination: Boolean): Float {
+        val e = estimators[instances[ix]]!!
+        val mean = e.mean
+        val score = if (elimination && e.nbrWeightedSamples < minSamples) Float.POSITIVE_INFINITY
+        else mean
+        return if (maximize) -score else score
+    }
+
+    fun estimator(position: Int): E? = estimators[instances[position]]
+
+    /**
+     * Updates the estimator of an instance with new data.
+     */
+    fun update(instance: Instance, result: Float, weight: Float) {
+        val e = estimators[instance] ?: return
+        val needUpdateMinMax = (e.mean == maxMean && result < e.mean) || (e.mean == minMean && result > e.mean)
+        banditPolicy.update(e, result, weight)
+        val newScore = e.mean
+        maxMean = max(maxMean, newScore)
+        minMean = min(minMean, newScore)
+        step++
+        if (needUpdateMinMax) calculateMinMax()
+    }
+
+    fun removeCandidate(position: Int) {
+        val oldInstance = instances[position]
+        instances = instances.removeAt(position)
+        origins = origins.removeAt(position)
+        unindexCandidate(position, oldInstance)
+    }
+
+    fun addCandidate(instance: Instance) {
+        instances += instance
+        origins += step
+        indexCandidate(instances.lastIndex) { banditPolicy.baseData() }
     }
 
     /**
-     * @return false if there are any duplicates left
+     * @return candidate if it is unique
      */
-    fun replaceCandidate(position: Int, instance: Instance, @Suppress("UNCHECKED_CAST") data: E = baseData.copy() as E): E? {
-        var last: E? = null
+    fun replaceCandidate(position: Int, instance: Instance): E? {
         val oldInstance = instances[position]
+        instances[position] = instance
+        origins[position] = step
+        val last = unindexCandidate(position, oldInstance)
+        indexCandidate(position) { banditPolicy.baseData() }
+        return last
+    }
+
+    fun replaceCandidates(newInstances: Array<Instance>, newData: Map<Instance, E>) {
+        instances = newInstances
+        origins = LongArray(newInstances.size) { step }
+        oldestOrigin = step
+        oldestCandidate = nbrCandidates - 1
+        estimators.clear()
+        duplications.clear()
+        minMean = Float.POSITIVE_INFINITY
+        maxMean = Float.NEGATIVE_INFINITY
+        for (i in instances.indices)
+            indexCandidate(i) { newData[instances[i]]!! }
+    }
+
+    fun isDuplicated(instance: Instance) = duplications[instance]?.size == 1
+
+    private inline fun indexCandidate(position: Int, data: () -> E) {
+        val e = estimators.getOrPut(instances[position]) {
+            duplications[instances[position]] = IntHashSet(nullValue = -1)
+            data()
+        }
+        // dont need to update oldestCandidate, since we are just adding new here
+        duplications[instances[position]]!!.add(position)
+        maxMean = max(maxMean, e.mean)
+        minMean = min(minMean, e.mean)
+    }
+
+    private fun unindexCandidate(position: Int, oldInstance: Instance): E? {
+        var last: E? = null
         val set = duplications[oldInstance]
         if (set != null) {
             set.remove(position)
@@ -54,35 +131,28 @@ class BanditCandidates<E : VarianceEstimator>(override val instances: Array<Muta
                 duplications.remove(oldInstance)
             }
         }
-        instances[position] = instance as MutableInstance
-        addCandidate(position, data)
-        origins[position] = nbrUpdates
-        if (oldestCandidate == position) {
-            oldestOrigin = Long.MAX_VALUE
-            for (i in origins.indices) {
-                if (origins[i] < oldestOrigin) {
-                    oldestOrigin = origins[i]
-                    oldestCandidate = i
-                    if (oldestOrigin == 0L) break
-                }
-            }
-        }
+        if (oldestCandidate == position) calculateOldest()
+        if (last != null && (last.mean == minMean || last.mean == maxMean)) calculateMinMax()
         return last
     }
 
-    private fun addCandidate(position: Int, data: E) {
-        estimators.getOrPut(instances[position]) {
-            duplications[instances[position]] = IntHashSet(nullValue = -1)
-            data
+    private fun calculateOldest() {
+        oldestOrigin = Long.MAX_VALUE
+        for (i in origins.indices) {
+            if (origins[i] < oldestOrigin) {
+                oldestOrigin = origins[i]
+                oldestCandidate = i
+                if (oldestOrigin == 0L) break
+            }
         }
-        duplications[instances[position]]!!.add(position)
     }
 
-    override fun score(ix: Int) = estimators[instances[ix]]?.mean ?: Float.NEGATIVE_INFINITY
-
-    fun update(newScore: Float) {
-        nbrUpdates++
-        if (newScore > maxScore || maxScore.isNaN()) maxScore = newScore
-        if (newScore < minScore || minScore.isNaN()) minScore = newScore
+    private fun calculateMinMax() {
+        minMean = Float.POSITIVE_INFINITY
+        maxMean = Float.NEGATIVE_INFINITY
+        for (e in estimators.values) {
+            minMean = min(minMean, e.mean)
+            maxMean = max(maxMean, e.mean)
+        }
     }
 }
