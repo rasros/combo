86da60b66ee70103c1fc15c63cda99f32c4d4d66
Rasmus Ros <rasmus.s.ros@gmail.com>
2019-09-29 20:42:44 +0200

    added method to measure millis (it is just defined on jvm in sdk)

--- common/src/main/kotlin/combo/util/Time.kt
+++ common/src/main/kotlin/combo/util/Time.kt
@@ -3,6 +3,12 @@ package combo.util
 expect fun nanos(): Long
 expect fun millis(): Long
 
+inline fun measureTimeMillis(block: () -> Unit): Long {
+    val start = millis()
+    block()
+    return millis() - start
+}
+
 inline fun measureTimeNanos(block: () -> Unit): Long {
     val start = nanos()
     block()
