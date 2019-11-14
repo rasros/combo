f3be6db26dc4c76d4bf86501eff0d66282ab2ad8
Rasmus Ros <rasmus.s.ros@gmail.com>
2019-11-14 12:20:30 +0100

    updated hyper parameters

--- common/src/main/kotlin/combo/model/BanditHyperParameters.kt
+++ common/src/main/kotlin/combo/model/BanditHyperParameters.kt
@@ -3,8 +3,10 @@ package combo.model
 import combo.bandit.BanditBuilder
 import combo.bandit.BanditData
 import combo.bandit.dt.*
+import combo.bandit.glm.CovarianceLinearModel
 import combo.bandit.glm.LinearBandit
 import combo.bandit.glm.LinearData
+import combo.bandit.glm.PrecisionLinearModel
 import combo.bandit.nn.NeuralLinearBandit
 import combo.bandit.nn.NeuralLinearData
 import combo.math.BinaryEstimator
@@ -25,9 +27,9 @@ interface BanditHyperParameters<D : BanditData> {
 class DecisionTreeHyperParameters(override val baseBuilder: DecisionTreeBandit.Builder) : BanditHyperParameters<TreeData> {
 
     override val metaModel = model {
-        int("delta", -300, 0) // 0..1
-        int("deltaDecay", -100, 10) // 0..10
-        int("tau", -100, 0) // 0..1
+        int("delta", -256, 0)
+        int("deltaDecay", -128, 127)
+        int("tau", -32, 0)
     }
 
     override fun toBandit(metaAssignment: Assignment) = baseBuilder
@@ -40,11 +42,11 @@ class RandomForestHyperParameters(override val baseBuilder: RandomForestBandit.B
 
     override val metaModel = model {
         if (baseBuilder.banditPolicy.prior is BinaryEstimator) nominal("splitMetric", EntropyReduction, ChiSquareTest, GiniCoefficient)
-        int("delta", -300, 0) // 0..1
-        int("deltaDecay", -100, 10) // 0..10
-        int("tau", -100, 0) // 0..1
-        int("samplingMean", -15, 15)
-        int("nbrVariables", -15, 0)
+        int("delta", -256, 0)
+        int("deltaDecay", -128, 127)
+        int("tau", -32, 0)
+        int("samplingMean", -8, 7)
+        int("nbrVariables", -16, 0)
     }
 
     override fun toBandit(metaAssignment: Assignment) = baseBuilder
@@ -55,46 +57,50 @@ class RandomForestHyperParameters(override val baseBuilder: RandomForestBandit.B
             .viewedVariables(max(1, (10f.pow(metaAssignment.getFloat("nbrVariables") / 10f) * baseBuilder.model.nbrVariables).toInt()))
 }
 
-class PrecisionLinearBanditHyperParameters(override val baseBuilder: LinearBandit.PrecisionBuilder) : BanditHyperParameters<LinearData> {
+class PrecisionLinearBanditHyperParameters(override val baseBuilder: LinearBandit.Builder) : BanditHyperParameters<LinearData> {
 
     override val metaModel = model {
-        int("precision", 0, 40) // 1..1e4
-        int("exploration", -60, 0) // 1e-6..1
-        int("regularizationFactor", -460, -10) // 0..1e-1f
+        int("precision", 0, 63)
+        int("exploration", -64, 0)
+        int("regularizationFactor", -256, -10)
     }
 
     override fun toBandit(metaAssignment: Assignment) = baseBuilder
-            .priorPrecision(10f.pow(metaAssignment.getInt("precision") / 10f))
-            .exploration(10f.pow(metaAssignment.getInt("exploration") / 10f))
-            .regularizationFactor(10f.pow(metaAssignment.getInt("regularizationFactor") / 10f))
+            .linearModel(PrecisionLinearModel.Builder(baseBuilder.problem)
+                    .family((baseBuilder.linearModel as PrecisionLinearModel).family)
+                    .priorPrecision(10f.pow(metaAssignment.getInt("precision") / 10f))
+                    .exploration(10f.pow(metaAssignment.getInt("exploration") / 10f))
+                    .regularizationFactor(10f.pow(metaAssignment.getInt("regularizationFactor") / 10f)).build())
 }
 
-class CovarianceLinearBanditHyperParameters(override val baseBuilder: LinearBandit.CovarianceBuilder) : BanditHyperParameters<LinearData> {
+class CovarianceLinearBanditHyperParameters(override val baseBuilder: LinearBandit.Builder) : BanditHyperParameters<LinearData> {
 
     override val metaModel = model {
-        int("variance", -40, 0) // 1e-4..1
-        int("exploration", -60, 0) // 1e-6..1
-        int("regularizationFactor", -460, -10) // 0..1e-1f
+        int("variance", -64, 0)
+        int("exploration", -64, 0)
+        int("regularizationFactor", -256, -10)
     }
 
     override fun toBandit(metaAssignment: Assignment) = baseBuilder
-            .priorVariance(10f.pow(metaAssignment.getInt("variance") / 10f))
-            .exploration(10f.pow(metaAssignment.getInt("exploration") / 10f))
-            .regularizationFactor(10f.pow(metaAssignment.getInt("regularizationFactor") / 10f))
+            .linearModel(CovarianceLinearModel.Builder(baseBuilder.problem)
+                    .family((baseBuilder.linearModel as CovarianceLinearModel).family)
+                    .priorVariance(10f.pow(metaAssignment.getInt("variance") / 10f))
+                    .exploration(10f.pow(metaAssignment.getInt("exploration") / 10f))
+                    .regularizationFactor(10f.pow(metaAssignment.getInt("regularizationFactor") / 10f)).build())
 }
 
 class NeuralLinearBanditHyperParameters(override val baseBuilder: NeuralLinearBandit.Builder) : BanditHyperParameters<NeuralLinearData> {
     override val metaModel = model {
-        nominal("regularizationFactor", 0f, 1e-6f, 1e-5f, 1e-4f, 1e-3f, 1e-2f, 1e-1f)
-        nominal("baseVariance", 1e-4f, 1e-3f, 1e-2f, 1e-1f, 1f, 1e1f, 1e2f, 1e3f, 1e4f)
-        nominal("varianceUpdateDecay", 0f, 1 - 1e-4f, 1 - 1e-3f, 1 - 1e-2f, 1 - 1e-1f)
-        nominal("weightUpdateDecay", 0f, 1 - 1e-4f, 1 - 1e-3f, 1 - 1e-2f, 1 - 1e-1f)
+        int("regularizationFactor", -256, -10)
+        int("baseVariance", -64, 63)
+        int("varianceUpdateDecay", -32, 0)
+        int("weightUpdateDecay", -32, 0)
     }
 
     override fun toBandit(metaAssignment: Assignment): BanditBuilder<NeuralLinearData> {
-        baseBuilder.networkBuilder.regularizationFactor(metaAssignment.getFloat("regularizationFactor"))
-        return baseBuilder.baseVariance(metaAssignment.getFloat("baseVariance"))
-                .varianceUpdateDecay(metaAssignment.getFloat("varianceUpdateDecay"))
-                .weightUpdateDecay(metaAssignment.getFloat("weightUpdateDecay"))
+        baseBuilder.networkBuilder.regularizationFactor(10f.pow(metaAssignment.getInt("regularizationFactor") / 10f))
+        return baseBuilder.baseVariance(10f.pow(metaAssignment.getInt("baseVariance") / 10f))
+                .varianceUpdateDecay(10f.pow(metaAssignment.getFloat("varianceUpdateDecay") / 10f))
+                .weightUpdateDecay(10f.pow(metaAssignment.getFloat("weightUpdateDecay") / 10f))
     }
 }
