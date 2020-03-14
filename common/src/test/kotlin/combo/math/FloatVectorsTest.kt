0d8e8b229919b5143ac9067c8a91197c363bbbf4
Rasmus Ros <rasmus.s.ros@gmail.com>
2020-03-14 23:21:30 +0100

    added test for sparsefloatvector

--- common/src/test/kotlin/combo/math/FloatVectorsTest.kt
+++ common/src/test/kotlin/combo/math/FloatVectorsTest.kt
@@ -1,4 +1,13 @@
 package combo.math
 
 class FloatVectorTest : VectorTest(FloatVectorFactory)
+class SparseFloatVectorTest : VectorTest(SparseFloatVectorFactory)
 class FloatMatrixTest : MatrixTest(FloatVectorFactory)
+
+object SparseFloatVectorFactory : VectorFactory {
+    override fun zeroVector(size: Int) = FloatSparseVector(size, FloatArray(size), IntArray(size) { it })
+    override fun vector(values: FloatArray) = FloatSparseVector(values.size, values, IntArray(values.size) { it })
+    override fun sparseVector(size: Int, values: FloatArray, indices: IntArray) = FloatSparseVector(size, values, indices)
+    override fun matrix(values: Array<FloatArray>) = FloatVectorFactory.matrix(values)
+    override fun zeroMatrix(rows: Int, columns: Int) = FloatVectorFactory.zeroMatrix(rows, columns)
+}
